7499775afbf90cad1d97352d758c858181321dcf
keonuk <keonuk22@naver.com>
2022-05-12 00:44:22 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  // root class
+  root: {
+    width: '100%',
+    // 위쪽 여백 3의 가중치
+    marginTop: theme.spacing.unit * 3,
+    // x축 오버플로우 발생 가능
+    overflow: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -31,29 +52,42 @@ const customers = [
 // 클래스 이용 컴포넌트 만들기
 class App extends React.Component{
   render(){
+    const {classes} = this.props;
     return(
-      <div>
-        {
-          // 특정 배열 각 원소에 접근해서 그 원소를 어떻게 처리할지(파이선과 문법 동일)
-          // map을 이용할때는 key={} props 사용해야함(콘솔 오류)
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-                />
-            ); 
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{
+              // 특정 배열 각 원소에 접근해서 그 원소를 어떻게 처리할지(파이선과 문법 동일)
+              // map을 이용할때는 key={} props 사용해야함(콘솔 오류)
+              customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                    />
+                ); 
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,38 +1,44 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-class Customer extends React.Component{
+class Customer extends React.Component{ 
     render(){ // 항상 수행되는 내용 Customer 컴포넌트를 화면에 그리고자 할때 실제로 그려지는 내용이 담김
         return(
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>                
+            </TableRow>
         )
     }
 }
 
-class CustomerProfile extends React.Component {
-    render() {
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
+// class CustomerProfile extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <img src={this.props.image} alt="profile"/>
+//                 <h2>{this.props.name}({this.props.id})</h2>
+//             </div>
+//         )
+//     }
+// }
 
-class CustomerInfo extends React.Component {
-    render() {
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
+// class CustomerInfo extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <p>{this.props.birthday}</p>
+//                 <p>{this.props.gender}</p>
+//                 <p>{this.props.job}</p>
 
-            </div>
-        )
-    }
-}
+//             </div>
+//         )
+//     }
+// }
 
 export default Customer;
