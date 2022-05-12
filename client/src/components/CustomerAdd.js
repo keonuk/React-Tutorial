184cd62988a3e37f5564e5a17d704ae9e1ff7a1e
keonuk <keonuk22@naver.com>
2022-05-13 01:44:39 +0900

    Customer Insert Database

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -22,6 +22,16 @@ class CustomerAdd extends React.Component {
             .then((response) => {
                 console.log(response.data);
             });
+            
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: ''
+        });
+        window.location.reload();
     }
 
     handleFileChange = (e) => {
--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'});
+
 app.get('/api/customers', (req, res) => {
     connection.query(
         "SELECT * FROM CUSTOMER",
@@ -29,6 +32,26 @@ app.get('/api/customers', (req, res) => {
             res.send(rows);
         }
     );
+});
+
+// image라는 이름의 경로로 접근을 하는데 실제 서버의 upload 폴더와 매핑이 됨
+app.use('/image', express.static('./upload'));
+
+// post 방식으로 경로에 사용자가 고객추가 데이터를 전송했을 때 처리
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename; // multer 라이브러리가 자동으로 겹치지않는 이름 핟당
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+
+    connection.query(sql, params, 
+        // 성공적으로 입력이 되었다면 관련 메시지를 client에게 출력
+        (err, rows, fields) => {
+            res.send(rows);
+        });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
