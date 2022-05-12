afe2b077477e1f8a4864ece3a65b912fc3f08a80
keonuk <keonuk22@naver.com>
2022-05-13 00:30:28 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const res = require('express/lib/response');
@@ -7,37 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get('/api/hello', (req, res) => {
-//     res.send({message: 'Hello Express!'});
-// });
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id' : '1',
-          'image' : 'https://placeimg.com/64/64/1',
-          'name' : '김철수',
-          'birthday' : '921104',
-          'gender' : '남자',
-          'job' : '대학생'
-        },
-        {
-          'id' : '2',
-          'image' : 'https://placeimg.com/64/64/2',
-          'name' : '홍길동',
-          'birthday' : '111111',
-          'gender' : '남자',
-          'job' : '디자이너'
-        },
-        {
-          'id' : '3',
-          'image' : 'https://placeimg.com/64/64/3',
-          'name' : '김길동',
-          'birthday' : '222222',
-          'gender' : '남자',
-          'job' : '의사'
-        },
-      ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
