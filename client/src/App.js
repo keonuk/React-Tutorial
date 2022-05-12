48459dbea9a3e53ed3e9a961c9eba38ca0db89e6
keonuk <keonuk22@naver.com>
2022-05-13 01:23:06 +0900

    Add Customer Insert Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -40,7 +41,7 @@ const styles = theme => ({
 });
 
 // 클래스 이용 컴포넌트 만들기
-class App extends React.Component{
+class App extends React.Component{ 
   
   state = {
     customers: "",
@@ -68,43 +69,46 @@ class App extends React.Component{
   render(){
     const {classes} = this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-              {/* 특정 배열 각 원소에 접근해서 그 원소를 어떻게 처리할지(파이선과 문법 동일) */}
-              {/* map을 이용할때는 key={} props 사용해야함(콘솔 오류) */}
-              {this.state.customers ? this.state.customers.map(c => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                    />
-                );   
-              }) : 
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                </TableCell>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
               </TableRow>
-              }
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+            <TableBody>
+                {/* 특정 배열 각 원소에 접근해서 그 원소를 어떻게 처리할지(파이선과 문법 동일) */}
+                {/* map을 이용할때는 key={} props 사용해야함(콘솔 오류) */}
+                {this.state.customers ? this.state.customers.map(c => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                      />
+                  );   
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+                }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd/>
+      </div>
     )
   }
 }
