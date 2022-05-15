efe2c49e45fe9b8705035747c2553bf1c028d79e
keonuk <keonuk22@naver.com>
2022-05-16 02:45:36 +0900

    Add AppBar & Web Fonts

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,23 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import {withStyles} from '@material-ui/core/styles';
+import  { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { styled, alpha } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+//import StyledInputBase from '@material-ui/core/InputBase';
+//import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+//import Search from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/Search';
+//import SearchIconWrapper from '@material-ui/icons/Search';
+
 
 /* 
   React LifeCycle
@@ -27,18 +42,67 @@ const styles = theme => ({
   // root class
   root: {
     width: '100%',
-    // 위쪽 여백 3의 가중치
-    marginTop: theme.spacing(3),
-    // x축 오버플로우 발생 가능
-    overflow: "auto"
-  },
-  table: {
     minWidth: 1080
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
   },
   progress: {
     margin: theme.spacing(2)
+  },
+  tableHead: {
+    fontSize: "1.0rem"
+  },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
   }
 });
+
+const Searchs = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrappers = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBases = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '20ch',
+      '&:focus': {
+        width: '40ch',
+      },
+    },
+  },
+}));
 
 // 클래스 이용 컴포넌트 만들기
 class App extends React.Component{ 
@@ -85,19 +149,52 @@ class App extends React.Component{
 
   render(){
     const {classes} = this.props;
+    const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"];
     return(
-      <div>
-        <Paper className={classes.root}>
+      <div className={classes.root}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+              >
+                고객 관리 시스템
+              </Typography>
+              <Searchs>
+                <SearchIconWrappers>
+                  <SearchIcon />
+                </SearchIconWrappers>
+                <StyledInputBases
+                  placeholder="           검색하기"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Searchs>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <div className={classes.menu}>
+          {/* 함수 자체를 props 형태로 보내줌 */}
+          <CustomerAdd stateRefresh={this.stateRefresh}/>
+        </div>
+        <Paper className={classes.paper}>
           <Table className={classes.table}>
             <TableHead>
               <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
+                {cellList.map(c => {
+                  return <TableCell className={classes.tableHead}>{c}</TableCell>
+                })}
               </TableRow>
             </TableHead>
             <TableBody>
@@ -126,8 +223,6 @@ class App extends React.Component{
             </TableBody>
           </Table>
         </Paper>
-        {/* 함수 자체를 props 형태로 보내줌 */}
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>
     )
   }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Noto Sans KR", serif',
+  }
+});
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root')
 );
